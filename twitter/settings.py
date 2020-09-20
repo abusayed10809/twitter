7341d30dfb5c2401b83b330848028e3bdf043f35
abusayed10809 <abusayed10809@gmail.com>
2020-09-20 12:16:22 +0600

    deployable

--- twitter/settings.py
+++ twitter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7=conqx@#i-2+^w&5f7894$__f$86@126+bd-_$jte(y#2*74g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '.mydomain.com']
+ALLOWED_HOSTS = ['localhost', 'https://twitter-10809.herokuapp.com/']
 LOGIN_URL = "/login"
 MAX_TWEET_LENGTH = 240
 TWEET_ACTION_OPTIONS = ["like", "unlike", "retweet"]
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_RENDERER_CLASSES = [
         'rest_framework.renderers.JSONRenderer',
